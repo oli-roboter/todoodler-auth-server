d51b0c019d5747629f1dbe36ffde3177cbbc3d51
Oliver Wagner <oliverthomas.wagner@gmail.com>
2020-06-12 21:45:14 +0100

    added salt and hashing to has-password function
    added decription function to hash-password

--- server/helpers/hash-password.js
+++ server/helpers/hash-password.js
@@ -1,3 +1,19 @@
-const hashPassword = (password) => password;
+import bcrypt from 'bcrypt';
 
-export default hashPassword;
+export default function hashPassword() {
+
+  const async hashPassword = (password) => {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(password, salt);
+  };
+
+  const async checkPassword = (requestPassword, password) => {
+    return await bcrypt.compare(requestPassword, password);
+  }
+
+  return Object.freeze({
+    hashPassword,
+    checkPassword
+  })
+
+}
