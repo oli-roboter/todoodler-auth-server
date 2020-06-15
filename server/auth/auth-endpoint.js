52ce037cab719184953cab86bc47d29298cc9f95
Oliver Wagner <oliverthomas.wagner@gmail.com>
2020-06-15 10:33:30 +0100

    completed token authorisation route

--- server/auth/auth-endpoint.js
+++ server/auth/auth-endpoint.js
@@ -1,23 +1,41 @@
+import winston from 'winston';
 import makeHttpError from '../helpers/http-error';
 
 export default function makeAuthEndpointHandler({ dbAuthHandler }) {
-  async function getUser(httpRequest) {
-    const { body } = httpRequest;
-    const { username } = body;
-    const users = await dbAuthHandler.getUser(username);
-    return {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      statusCode: 200,
-      data: JSON.stringify({ users }),
-    };
+  async function checkUserToken(httpRequest) {
+    try {
+      const { body } = httpRequest;
+      const { username, token } = body;
+      const users = await dbAuthHandler.getUser(username);
+      const isTokenValid = token === users.token;
+      winston.info('User token validated');
+      if (isTokenValid) {
+        return {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          statusCode: 200,
+          data: isTokenValid,
+        };
+      }
+      winston.warn('User token is not valid');
+      return makeHttpError({
+        statusCode: 403,
+        errorMessage: 'Not authorized.',
+      });
+    } catch (e) {
+      winston.error(e);
+      return makeHttpError({
+        statusCode: 400,
+        errorMessage: 'Bad request. GET body must be valid JSON.'
+      });
+    }
   }
 
   return async function handle(httpRequest) {
     switch (httpRequest.method) {
       case 'GET':
-        return getUser(httpRequest);
+        return checkUserToken(httpRequest);
       default:
         return makeHttpError({
           statusCode: 405,
--- server/auth/auth-methods.js
+++ server/auth/auth-methods.js
@@ -3,15 +3,15 @@ import winston from 'winston';
 
 export default function dbMethods({ database }) {
   const getUser = async (username) => {
-    winston.info('Getting user');
+    winston.info('Getting user token');
     const db = await database();
     const result = await db
-      .collection('users')
+      .collection('tokens')
       .find({ username })
       .toArray();
 
     return result;
-  }
+  };
 
   return Object.freeze({
     getUser,
