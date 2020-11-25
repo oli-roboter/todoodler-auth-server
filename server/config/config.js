77861884e8dd38eb40c5f034dea90e1cc73c57bf
Oliver Wagner <oliverthomas.wagner@gmail.com>
2020-11-25 16:48:42 +0000

    changed port env variable to run in production

--- server/config/config.js
+++ server/config/config.js
@@ -2,9 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { PORT, NODE_ENV } = process.env;
+const { AUTH_PORT, NODE_ENV, MONGO_ATLAS_URL } = process.env;
 // const MONGO_DB = process.env.USERS_DB_URL;
-const MONGO_DB = process.env.MONGO_ATLAS_URL;
+const MONGO_DB = MONGO_ATLAS_URL;
+const PORT = AUTH_PORT;
+console.log("MONGO_DB:", MONGO_DB);
+console.log("PORT:", PORT);
 
 export {
   PORT,
