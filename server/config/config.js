ccaa42c1776083ff562174c0424701723ea30287
Oliver Wagner <oliverthomas.wagner@gmail.com>
2020-11-25 13:44:34 +0000

    Changed environment variable name to connect to mongoAtlas in production

--- server/config/config.js
+++ server/config/config.js
@@ -3,12 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { PORT, NODE_ENV } = process.env;
-const MONGO_DB = process.env.USERS_DB_URL;
-const AUTH_DB = process.env.USERS_DB_NAME;
-console.log('MOngoloide DB', MONGO_DB);
+// const MONGO_DB = process.env.USERS_DB_URL;
+const MONGO_DB = process.env.MONGO_ATLAS_URL;
+
 export {
   PORT,
   NODE_ENV,
   MONGO_DB,
-  AUTH_DB,
 };
