62036095c974c29e8b639706e87fb53ae4cde505
Oliver Wagner <oliverthomas.wagner@gmail.com>
2020-11-25 21:07:38 +0000

    deleted debug messages

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.confiqg();
 
 const { AUTH_PORT, NODE_ENV, MONGO_ATLAS_URL } = process.env;
 // const MONGO_DB = process.env.USERS_DB_URL;
 const MONGO_DB = MONGO_ATLAS_URL;
 const PORT = AUTH_PORT;
-console.log("MONGO_DB:", MONGO_DB);
-console.log("PORT:", PORT);
+// console.log("MONGO_DB:", MONGO_DB);
+// console.log("PORT:", PORT);
+// console.log('PROCESS ENV', process.env);
 
 export {
   PORT,
